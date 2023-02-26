f8100d7bf17e0ef3c0c0541966092452f3f7fb81
lorenaneda <nedalorena2000@gmail.com>
2023-02-26 12:41:11 +0100

    mise en place du chart js

---
+++ colorschemes/chartjs-plugin-colorschemes-master/test/fixtures/plugin.colorschemes/chart-bar.js
@@ -0,0 +1,38 @@
+export default {
+	config: {
+		type: 'bar',
+		data: {
+			labels: [0, 1, 2, 3, 4, 5],
+			datasets: [{
+				data: [0, 5, 10, 0, -10, -5]
+			}, {
+				data: [10, 0, -10, -5, 0, 5]
+			}, {
+				data: [-10, -5, 0, 5, 10, 0]
+			}]
+		},
+		options: {
+			legend: false,
+			title: false,
+			layout: {
+				padding: 4
+			},
+			scales: {
+				xAxes: [{display: false}],
+				yAxes: [{display: false}]
+			},
+			plugins: {
+				colorschemes: {
+					scheme: 'brewer.Accent3',
+					fillAlpha: 1
+				}
+			}
+		}
+	},
+	options: {
+		canvas: {
+			height: 256,
+			width: 512
+		}
+	}
+};
--- js/javascript.js
+++ js/javascript.js
@@ -10,3 +10,33 @@ lottie.loadAnimation({
 });
 
 
+/*
+var customColorFunction = function (schemeColors) {
+  var myColors = ['#3DB2FF', '#FFEDDA', '#FFB830', '#FF2442']; // define/generate own colors
+
+  // extend the color scheme with own colors
+  Array.prototype.push.apply(schemeColors, myColors);
+
+  return schemeColors; // optional: this is not needed if the array is modified in place
+};
+*/
+var ctx = document.getElementById('chartjs').getContext('2d');
+
+var data = {
+  labels: ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'],
+  datasets: [{
+    label: 'Moyenne des assecs par année',
+    data: [4, 1.2, 1.4, 3.2, 1, 5.714285714, 6.75, 6.75, 7.214285714, 0.5, 4.714285714],
+  }]
+};
+
+var options = {
+  responsive: true,
+  maintainAspectRatio: false
+};
+
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: data,
+  options: options
+});
